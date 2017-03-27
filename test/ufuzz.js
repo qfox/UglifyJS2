72514d320b0b22fd635625e2cfd278d88c91bed2
Peter van der Zee <github-public@qfox.nl>
2017-03-27 23:07:58 +0200

    Removing some training wheels; dont add parens where we dont absolutely need them

--- test/ufuzz.js
+++ test/ufuzz.js
@@ -373,7 +373,7 @@ function createExpression(recurmax, noComma) {
     }
     // since `a` and `b` are our canaries we want them more frequently than other expressions (1/3rd chance of a canary)
     let r = rng(6);
-    if (r < 1) return '(a++) + ' + _createExpression(recurmax, noComma);
+    if (r < 1) return 'a++ + ' + _createExpression(recurmax, noComma);
     if (r < 2) return '(--b) + ' + _createExpression(recurmax, noComma);
     if (r < 3) return '(c = c + 1) + ' + _createExpression(recurmax, noComma); // c only gets incremented
 
@@ -382,13 +382,14 @@ function createExpression(recurmax, noComma) {
 function _createExpression(recurmax, noComma) {
     switch (rng(12)) {
         case 0:
-            return '(' + createUnaryOp() + (rng(2) === 1 ? 'a' : 'b') + ')';
+            return createUnaryOp() + (rng(2) === 1 ? 'a' : 'b');
         case 1:
-            return '(a' + (rng(2) == 1 ? '++' : '--') + ')';
+            return 'a' + (rng(2) == 1 ? '++' : '--');
         case 2:
+            // parens needed because assignments aren't valid unless they're the left-most op(s) in an expression
             return '(b ' + createAssignment() + ' a)';
         case 3:
-            return '(' + rng(2) + ' === 1 ? a : b)';
+            return rng(2) + ' === 1 ? a : b';
         case 4:
             return createExpression(recurmax, noComma) + createBinaryOp(noComma) + createExpression(recurmax, noComma);
         case 5:
@@ -396,7 +397,7 @@ function _createExpression(recurmax, noComma) {
         case 6:
             return '(' + createExpression(recurmax) + ')';
         case 7:
-            return createExpression(recurmax, noComma) + '?(' + createExpression(recurmax) + '):(' + createExpression(recurmax) + ')';
+            return createExpression(recurmax, noComma) + '?' + createExpression(recurmax, NO_COMMA) + ':' + createExpression(recurmax, noComma);
         case 8:
             let name = createVarName(MAYBE);
             if (name === 'c') name = 'a';
@@ -413,7 +414,7 @@ function _createExpression(recurmax, noComma) {
                     return 'void function ' + name + '(){' + createStatements(rng(5) + 1, recurmax) + '}';
             }
         case 9:
-            return createTypeofExpr(recurmax);
+            return createTypeofExpr();
         case 10:
             // you could statically infer that this is just `Math`, regardless of the other expression
             // I don't think Uglify does this at this time...
@@ -424,11 +425,13 @@ function _createExpression(recurmax, noComma) {
                 '}';
         case 11:
             // more like a parser test but perhaps comment nodes mess up the analysis?
+            // note: parens not needed for post-fix (since that's the default when ambiguous)
+            // for prefix ops we need parens to prevent accidental syntax errors.
             switch (rng(6)) {
                 case 0:
-                    return '(a/* ignore */++)';
+                    return 'a/* ignore */++';
                 case 1:
-                    return '(b/* ignore */--)';
+                    return 'b/* ignore */--';
                 case 2:
                     return '(++/* ignore */a)';
                 case 3:
@@ -439,29 +442,25 @@ function _createExpression(recurmax, noComma) {
                     return '(--(b))';
                 case 5:
                     // classic 0.3-0.1 case; 1-0.1-0.1-0.1 is not 0.7 :)
-                    return '(b + 1-0.1-0.1-0.1)';
+                    return 'b + 1-0.1-0.1-0.1';
                 default:
                     return '(--/* ignore */b)';
             }
     }
 }
 
-function createTypeofExpr(recurmax) {
-    if (--recurmax < 0) {
-        return 'typeof undefined === "undefined"';
-    }
-
+function createTypeofExpr() {
     switch (rng(5)) {
         case 0:
-            return '(typeof ' + createVarName() + ' === "' + TYPEOF_OUTCOMES[rng(TYPEOF_OUTCOMES.length)] + '")';
+            return 'typeof ' + createVarName() + ' === "' + TYPEOF_OUTCOMES[rng(TYPEOF_OUTCOMES.length)] + '"';
         case 1:
-            return '(typeof ' + createVarName() + ' !== "' + TYPEOF_OUTCOMES[rng(TYPEOF_OUTCOMES.length)] + '")';
+            return 'typeof ' + createVarName() + ' !== "' + TYPEOF_OUTCOMES[rng(TYPEOF_OUTCOMES.length)] + '"';
         case 2:
-            return '(typeof ' + createVarName() + ' == "' + TYPEOF_OUTCOMES[rng(TYPEOF_OUTCOMES.length)] + '")';
+            return 'typeof ' + createVarName() + ' == "' + TYPEOF_OUTCOMES[rng(TYPEOF_OUTCOMES.length)] + '"';
         case 3:
-            return '(typeof ' + createVarName() + ' != "' + TYPEOF_OUTCOMES[rng(TYPEOF_OUTCOMES.length)] + '")';
+            return 'typeof ' + createVarName() + ' != "' + TYPEOF_OUTCOMES[rng(TYPEOF_OUTCOMES.length)] + '"';
         case 4:
-            return '(typeof ' + createVarName() + ')';
+            return 'typeof ' + createVarName();
     }
 }
 
